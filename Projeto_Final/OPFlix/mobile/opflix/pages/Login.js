d0d24e8f42a08184b60ede758dca6c386bb6b4d4
camargogigi <48352841+camargogigi@users.noreply.github.com>
2019-11-11 12:32:41 -0200

    up

---
+++ Projeto_Final/OPFlix/mobile/opflix/pages/Login.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
+
+class Login extends Component{
+
+    constructor(){
+        super();
+        this.state={
+            email: null,
+            senha: null
+        };
+    }
+
+
+
+}
+
+export default Login
